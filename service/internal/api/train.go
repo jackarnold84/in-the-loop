e7eb471c54847921e1464aa428124dc83e2c7ca0
Jack Arnold <jackarnold84@gmail.com>
2024-07-19 10:18:15 -0500

    improve api model

--- service/internal/api/train.go
+++ service/internal/api/train.go
@@ -3,6 +3,7 @@ package api
 import (
 	"fmt"
 	"multi-lambda/internal/env"
+	"multi-lambda/internal/model"
 )
 
 const (
@@ -10,7 +11,7 @@ const (
 	trainFollowURL   = "http://lapi.transitchicago.com/api/1.0/ttfollow.aspx"
 )
 
-type TrainArrival struct {
+type trainArrival struct {
 	StopID          string `json:"stpId"`
 	Route           string `json:"rt"`
 	RunNumber       string `json:"rn"`
@@ -20,6 +21,7 @@ type TrainArrival struct {
 	ArrivalTime     string `json:"arrT"`
 	Approaching     string `json:"isApp"`
 	Delayed         string `json:"isDly"`
+	Scheduled       string `json:"isSch"`
 }
 
 type trainApiResponse struct {
@@ -27,11 +29,32 @@ type trainApiResponse struct {
 }
 
 type trainArrivalResponse struct {
-	Data      []TrainArrival `json:"eta"`
+	Data      []trainArrival `json:"eta"`
 	ErrorName string         `json:"errNm"`
 }
 
-func TrainStopArrivals(stopID string) ([]TrainArrival, error) {
+func (a trainArrival) toVehicle() model.Vehicle {
+	return model.Vehicle{
+		TransitType: "train",
+		Route:       toTrainRoute(a.Route),
+		Run:         a.RunNumber,
+		Destination: a.DestinationName,
+		Direction:   toDirectionString(a.DirectionCode),
+	}
+}
+
+func (a trainArrival) toArrival() model.Arrival {
+	return model.Arrival{
+		StopID:        a.StopID,
+		StopName:      a.StationName,
+		Time:          a.ArrivalTime,
+		IsApproaching: a.Approaching == "1",
+		IsDelayed:     a.Delayed == "1",
+		IsLive:        a.Scheduled == "0",
+	}
+}
+
+func trainStopArrivals(stopID string) ([]model.VehicleArrival, error) {
 	params := map[string]string{
 		"stpid":      stopID,
 		"key":        env.Cfg.TrainAPIKey,
@@ -46,27 +69,71 @@ func TrainStopArrivals(stopID string) ([]TrainArrival, error) {
 	if response.Resp.ErrorName != "" {
 		return nil, fmt.Errorf("train API error: %s", response.Resp.ErrorName)
 	}
-	arrivals := response.Resp.Data
+	trainArrivals := response.Resp.Data
 
+	var arrivals []model.VehicleArrival
+	for _, train := range trainArrivals {
+		arrivals = append(arrivals, model.VehicleArrival{
+			Vehicle: train.toVehicle(),
+			Arrival: train.toArrival(),
+		})
+	}
 	return arrivals, nil
 }
 
-func TrainFollowArrivals(runNumber string) ([]TrainArrival, error) {
+func trainFollowArrivals(run string) (model.VehicleRoute, error) {
 	params := map[string]string{
-		"runnumber":  runNumber,
+		"runnumber":  run,
 		"key":        env.Cfg.TrainAPIKey,
 		"outputType": "JSON",
 	}
 
 	var response trainApiResponse
-	err := getApiResponse(trainArrivalsURL, params, &response)
+	err := getApiResponse(trainFollowURL, params, &response)
 	if err != nil {
-		return nil, err
+		return model.VehicleRoute{}, err
 	}
 	if response.Resp.ErrorName != "" {
-		return nil, fmt.Errorf("train API error: %s", response.Resp.ErrorName)
+		return model.VehicleRoute{}, fmt.Errorf("train API error: %s", response.Resp.ErrorName)
 	}
-	arrivals := response.Resp.Data
+	trainArrivals := response.Resp.Data
+	if len(trainArrivals) == 0 {
+		return model.VehicleRoute{}, fmt.Errorf("no arrivals found for vehicle %s", run)
+	}
 
-	return arrivals, nil
+	vehicle := trainArrivals[0].toVehicle()
+	var arrivals []model.Arrival
+	for _, train := range trainArrivals {
+		arrivals = append(arrivals, train.toArrival())
+	}
+	return model.VehicleRoute{
+		Vehicle:  vehicle,
+		Arrivals: arrivals,
+	}, nil
+}
+
+func toDirectionString(directionCode string) string {
+	switch directionCode {
+	case "1":
+		return "Northbound"
+	case "5":
+		return "Southbound"
+	default:
+		return directionCode
+	}
+}
+
+func toTrainRoute(route string) string {
+	switch route {
+	case "Brn":
+		return "Brown"
+	case "G":
+		return "Green"
+	case "Org":
+		return "Orange"
+	case "P":
+		return "Purple"
+	default:
+		return route
+	}
 }
---
+++ service/internal/track/track_test.go
@@ -0,0 +1,69 @@
+package track
+
+import (
+	"fmt"
+	"multi-lambda/internal/model"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetTrips(t *testing.T) {
+	t.Run("train", func(t *testing.T) {
+		// green+pink lines clinton to clark/lake
+		trips, err := TrackArrivals("train", []string{"Green", "Pink"}, "30221", "30074")
+		assert.NoError(t, err)
+		assert.NotEmpty(t, trips)
+
+		for _, trip := range trips {
+			assert.Equal(t, "Clinton", trip.Arrival.StopName)
+			if trip.Departure != (model.Arrival{}) {
+				assert.Equal(t, "Clark/Lake", trip.Departure.StopName)
+				assert.True(t, trip.Departure.Time > trip.Arrival.Time)
+			}
+		}
+	})
+
+	t.Run("bus", func(t *testing.T) {
+		// 9 bus ashland/adams to ashland/lake
+		trips, err := TrackArrivals("bus", []string{"9"}, "17177", "14783")
+		assert.NoError(t, err)
+		assert.NotEmpty(t, trips)
+
+		fmt.Println(trips)
+
+		for _, trip := range trips {
+			assert.Equal(t, "Ashland & Adams", trip.Arrival.StopName)
+			if trip.Departure != (model.Arrival{}) {
+				assert.Equal(t, "Ashland & Lake (Green Line)", trip.Departure.StopName)
+				assert.True(t, trip.Departure.Time > trip.Arrival.Time)
+			}
+		}
+	})
+}
+
+func TestFollowVehicle(t *testing.T) {
+	t.Run("train", func(t *testing.T) {
+		// brown line merchanise mart northbound
+		trips, err := TrackArrivals("train", []string{"Brown"}, "30090", "30155")
+		assert.NoError(t, err)
+		assert.NotEmpty(t, trips)
+
+		run := trips[0].Run
+		vehicleRoute, err := FollowVehicle("train", run)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, vehicleRoute.Arrivals)
+	})
+
+	t.Run("bus", func(t *testing.T) {
+		// 20 bus madison/ashland to washington/clark
+		trips, err := TrackArrivals("bus", []string{"20"}, "432", "18122")
+		assert.NoError(t, err)
+		assert.NotEmpty(t, trips)
+
+		run := trips[0].Run
+		vehicleRoute, err := FollowVehicle("bus", run)
+		assert.NoError(t, err)
+		assert.NotEmpty(t, vehicleRoute.Arrivals)
+	})
+}
